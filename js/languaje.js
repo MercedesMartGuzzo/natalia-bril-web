9296998793dc019792dd542a0a7a61cfce38422e
Mercedes Martinez <mercedesviolin@gmail.com>
2024-09-23 21:23:15 -0300

    Cambiando posicion de titulos dr portadas mobile

--- js/languaje.js
+++ js/languaje.js
@@ -1,17 +1,17 @@
-/*  const userLang = navigator.language || navigator.userLanguage; 
+const userLang = navigator.language || navigator.userLanguage;
 
 // determines if the user has a set theme
-function detectLanguage(){
+function detectLanguage() {
     // local storage is used to override OS theme settings
-    if(localStorage.getItem("language")){
-        if(localStorage.getItem("language") == "es"){
+    if (localStorage.getItem("language")) {
+        if (localStorage.getItem("language") == "es") {
             return
         } else {
-            document.location.href="./en";
+            document.location.href = "./en";
         }
     } else {
         if (!userLang.startsWith("es")) {
-            document.location.href="./en";
+            document.location.href = "./en";
         }
     }
 }
@@ -21,4 +21,4 @@ const buttonLanguage = document.querySelector("#button-language");
 
 buttonLanguage.addEventListener("click", () => {
     localStorage.setItem('language', 'en');
-})  */
+})  
